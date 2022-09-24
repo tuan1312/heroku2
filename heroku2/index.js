e0a1d9a0b33934fb98b8edb6c1f730bbe7e32af9
tuan1312 <113591813+tuan1312@users.noreply.github.com>
2022-09-24 14:19:23 +0700

    Add files via upload

--- heroku2/index.js
+++ heroku2/index.js
@@ -4,7 +4,13 @@ const port = process.env.PORT || 3000
 const index = http.createServer((request, Response) => {
     Response.statuscode = 200
     Response.setHeader("Content-Type", "text/html")
-    Response.end("Hello everybody. I had deployed NodeJS to Heroku succeed !")
+    Response.write("<h1>Hello everyone !</h1>")
+    Response.write("<h2>I am learning NodeJS</h2>")
+    Response.write(
+        "<h3>My web page has been deployed to Heroku successfully !</h3>"
+      )
+      Response.write("<img src=https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4wtcN?ver=7464>")
+    Response.end()
 })
 index.listen(port, () => {
     console.log("erver is listening at http://localhost:" + port)
